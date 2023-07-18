d292fe37012166396a08be70b355e02a5504beba
rlskoeser <rebecca.s.koeser@princeton.edu>
2023-07-18 12:59:21 -0400

    Use more efficient random method for weighted coin flip

--- simulatingrisk/utils.py
+++ simulatingrisk/utils.py
@@ -1,7 +1,7 @@
 import random
 
 
-def coinflip(choices: [any, any] = [0, 1], weight: float = None) -> any:
+def coinflip(choices: [any, any] = [0, 1], weight: float = 0.5) -> any:
     """Flip a coin with an optional weight between 0.0 and 1.0 for the
     first choice. If no  weight is specified, a choice is made with
     equal probability.
@@ -9,14 +9,13 @@ def coinflip(choices: [any, any] = [0, 1], weight: float = None) -> any:
     :param choices: list of coin flip options, defaults to [0, 1]
     :type choices: [any, any] (optional)
     :param weight: optional weight between 0.0-1.0 for the first
-        choice, defaults to None
-    :type weight: float (optional)
+        choice, defaults to 0.5
+    :type weight: float (toptional)
 
     :return: selected choice
     :rtype: any
     """
-    options = {}
-    if weight:
-        options["weights"] = [weight, 1 - weight]
-    # random.choices sorts options based on weight; return first choice
-    return random.choices(choices, **options)[0]
+    # adapted from https://stackoverflow.com/a/477248/9706217
+    # random.random is apparently faster than
+    selection = 0 if random.random() < weight else 1
+    return choices[selection]
