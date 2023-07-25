75e919fbe43756cea781c7a04522f720de19e3dc
rlskoeser <rebecca.s.koeser@princeton.edu>
2023-07-25 14:21:19 -0400

    Split out server setup from server running code; add histogram #10

--- simulatingrisk/risky_food/run.py
+++ simulatingrisk/risky_food/run.py
@@ -1,38 +1,18 @@
 import mesa
 
 from simulatingrisk.risky_food.model import RiskyFoodModel
-
-
-chart = mesa.visualization.ChartModule(
-    [
-        {"Label": "prob_notcontaminated", "Color": "blue"},
-        {"Label": "contaminated", "Color": "red"},
-    ],
-    data_collector_name="datacollector",
-    canvas_height=100,  # default height is 200
-)
-risk_chart = mesa.visualization.ChartModule(
-    [
-        {"Label": "average_risk_level", "Color": "blue"},
-        {"Label": "min_risk_level", "Color": "green"},
-        {"Label": "max_risk_level", "Color": "orange"},
-    ],
-    data_collector_name="datacollector",
-    canvas_height=100,
+from simulatingrisk.risky_food.server import (
+    chart,
+    risk_chart,
+    total_agent_chart,
+    histogram,
 )
 
-total_agent_chart = mesa.visualization.ChartModule(
-    [
-        {"Label": "num_agents", "Color": "gray"},
-    ],
-    data_collector_name="datacollector",
-    canvas_height=100,
-)
 
 server = mesa.visualization.ModularServer(
     RiskyFoodModel,
     # [chart, risk_chart, agent_risk_chart, total_agent_chart],
-    [chart, risk_chart, total_agent_chart],
+    [chart, risk_chart, total_agent_chart, histogram],
     "Risky Food",
     {"n": 20, "mode": "types"},
 )
