534989ddb5e9a86362a9310463c779fb04ca9415
rlskoeser <rebecca.s.koeser@princeton.edu>
2023-08-08 11:43:12 -0400

    Tweak risky bet model for running with solara / jupyterviz

--- simulatingrisk/risky_bet/run.py
+++ simulatingrisk/risky_bet/run.py
@@ -1,30 +1,14 @@
 import mesa
 
 from simulatingrisk.risky_bet.model import RiskyBetModel, divergent_colors
-from simulatingrisk.risky_bet.server import agent_portrayal
+from simulatingrisk.risky_bet.server import (
+    agent_portrayal,
+    grid_size,
+    model_params,
+    risk_bins,
+)
 from simulatingrisk.charts.histogram import RiskHistogramModule
 
-
-grid_size = 20
-
-
-# make model parameters user-configurable
-model_params = {
-    "grid_size": grid_size,  # mesa.visualization.StaticText(value=grid_size),
-    # "grid_size": mesa.visualization.Slider(
-    #     "Grid size",
-    #     value=20,
-    #     min_value=10,
-    #     max_value=100,
-    #     description="Grid dimension (n*n = number of agents)",
-    # ),
-    "risk_adjustment": mesa.visualization.Choice(
-        "Risk adjustment strategy",
-        value="adopt",
-        choices=["adopt", "average"],
-        description="How agents update their risk level",
-    ),
-}
 
 grid = mesa.visualization.CanvasGrid(agent_portrayal, grid_size, grid_size, 500, 500)
 
@@ -49,14 +33,7 @@ world_chart = mesa.visualization.ChartModule(
 )
 
 
-# generate bins for histogram, capturing 0-0.5 and 0.95-1.0
-risk_bins = []
-r = 0.05
-while r < 1.05:
-    risk_bins.append(round(r, 2))
-    r += 0.1
 histogram = RiskHistogramModule(risk_bins, 175, 500, "risk levels")
-
 
 server = mesa.visualization.ModularServer(
     RiskyBetModel,
