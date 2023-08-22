61e0ff5902e42aa9e64bffee285e60b442469fc5
rlskoeser <rebecca.s.koeser@princeton.edu>
2023-08-22 18:04:36 -0400

    Preliminary solara / jupyterviz app for hawkdove sim

--- simulatingrisk/hawkdove/server.py
+++ simulatingrisk/hawkdove/server.py
@@ -29,6 +29,8 @@ def agent_portrayal(agent):
     else:
         colors = divergent_colors_5
     portrayal["Color"] = colors[agent.risk_level]
+    # copy to lowercase color for solara
+    portrayal["color"] = portrayal["Color"]
 
     # filled for hawks, hollow for doves
     # (shapes would be better...)
@@ -39,8 +41,8 @@ def agent_portrayal(agent):
         # # set radius based on relative points, but don't go smaller than 1 radius
         # # or too large to fit in the grid
         portrayal["r"] = (agent.points_rank / 15) + 0.2
-        # # size for solara / jupyterviz TODO
-        # portrayal["size"] = (wealth_index / 15) * 50
+        # # size for solara / jupyterviz
+        portrayal["size"] = (agent.points_rank / 15) * 50
 
     return portrayal
 
@@ -49,4 +51,22 @@ grid_size = 10
 
 model_params = {
     "grid_size": grid_size,
+}
+
+
+jupyterviz_params = {
+    "grid_size": {
+        "type": "SliderInt",
+        "value": grid_size,
+        "label": "Grid Size",
+        "min": 10,
+        "max": 100,
+        "step": 1,
+    },
+    # "risk_adjustment": {
+    #     "type": "Select",
+    #     "value": "adopt",
+    #     "values": ["adopt", "average"],
+    #     "description": "How agents update their risk level",
+    # },
 }
