634edc06bb0e69ad9320deddeccaf9e089fa6438
rlskoeser <rebecca.s.koeser@princeton.edu>
2023-09-26 11:29:04 -0400

    Preliminary batch run for hawk/dove with single risk attitude

--- simulatingrisk/batch_run.py
+++ simulatingrisk/batch_run.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 from mesa import batch_run
 
+from simulatingrisk.hawkdove.model import HawkDoveModel
 from simulatingrisk.risky_bet.model import RiskyBetModel
 from simulatingrisk.risky_food.model import RiskyFoodModel
 
@@ -46,6 +47,28 @@ def riskyfood_batch_run():
     return results
 
 
+def hawkdove_batch_run():
+    # params are:
+    # grid_size,
+    # include_diagonals=True,
+    # risk_attitudes="variable", or single
+    # agent_risk_level=None,
+    # hawk_odds=0.5,
+    return batch_run(
+        HawkDoveModel,
+        # when including diagonals, risk levels go from 0 to 8;
+        # probably do not need to include the extremes for this analysis
+        parameters={
+            "grid_size": 20,
+            "risk_attitudes": "single",
+            "agent_risk_level": [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        },
+        number_processes=1,
+        data_collection_period=1,
+        display_progress=True,
+    )
+
+
 def save_results(simulation, results):
     # save as csv for external analysis
     # - use datetime to distinguish this run, but make nicer for filename
@@ -68,12 +91,14 @@ if __name__ == "__main__":
         prog="simulatingrisk batch_run",
         description="Run simulations in batch mode and save collected data",
     )
-    parser.add_argument("simulation", choices=["riskybet", "riskyfood"])
+    parser.add_argument("simulation", choices=["riskybet", "riskyfood", "hawkdove"])
     args = parser.parse_args()
 
     if args.simulation == "riskybet":
         results = riskybet_batch_run()
     elif args.simulation == "riskyfood":
         results = riskyfood_batch_run()
+    elif args.simulation == "hawkdove":
+        results = hawkdove_batch_run()
 
     save_results(args.simulation, results)
