272c80fd6d8d8aa90427b3c3f4a129783f34d536
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-07-13 17:38:18 -0400

    Payoff analysis

--- notebooks/multi/payoffs.py
+++ notebooks/multi/payoffs.py
@@ -174,7 +174,136 @@ def _(mo):
     ## Payoff and simulation parameters
 
     How is agent payoff affected by simulation parameters?
+    """)
+    return
 
+
+@app.cell
+def _(model_agent_df, params, pl):
+    # one row per run_id — mean scaled_points across all agents in that run
+
+    # parameters to test correlation
+    corr_variables = ["q1_payoff", "mean_payoff", "q3_payoff"]
+
+    param_variables = [
+        p
+        for p in params["no_adjustment"].keys()
+        if p not in ["risk_distribution", "risk_adjustment"]
+    ]
+
+    # risk distribution is categorical; treat to one-hot 0/1 for each
+    _model_agent_riskdist_df = model_agent_df.to_dummies("risk_distribution")
+    # print(_model_agent_riskdist_df.columns)
+    param_variables.extend(
+        [
+            c
+            for c in _model_agent_riskdist_df.columns
+            if c.startswith("risk_distribution_")
+        ]
+    )
+    # print(corr_variables)
+
+    # corr_sample_size = 100_000   # completes in 17.20s
+    corr_sample_size = 1_000_000  # completes in 32.69s
+
+    param_level = (
+        _model_agent_riskdist_df.sample(corr_sample_size)
+        .group_by("run_id", *param_variables)
+        .agg(
+            mean_payoff=pl.col.scaled_points.mean(),
+            q1_payoff=pl.col.scaled_points.quantile(0.25),
+            q3_payoff=pl.col.scaled_points.quantile(0.75),
+        )
+    )
+
+    # param_level
+    param_level
+    # corr = param_level.corr()
+
+    return corr_sample_size, corr_variables, param_level, param_variables
+
+
+@app.cell(hide_code=True)
+def _(corr_variables, param_level, param_variables, pl):
+    _corr_cols = corr_variables + param_variables
+
+    run_corr_matrix_df = (
+        param_level.select(_corr_cols)
+        # add labels
+        .corr()
+        .with_columns(pl.Series("var_x", _corr_cols))
+        .select("var_x", *_corr_cols)
+    )
+    run_corr_matrix_df
+    return (run_corr_matrix_df,)
+
+
+@app.cell
+def _(pl, run_corr_matrix_df):
+
+    # add columns as label, then unpivot to long form for charting
+    run_corr_matrix_long_df = (
+        run_corr_matrix_df.unpivot(
+            on=run_corr_matrix_df.columns,
+            index="var_x",
+            variable_name="var_y",
+            value_name="correlation",
+        )
+        .filter(pl.col.var_y.is_in(["mean_payoff", "q1_payoff", "q3_payoff"]))
+        .filter(~pl.col.var_x.is_in(["mean_payoff", "q1_payoff", "q3_payoff"]))
+        .with_columns(
+            correlation=pl.col.correlation.cast(pl.Float32),
+            correlation_str=pl.col.correlation.map_elements(
+                lambda x: f"{float(x):,.2f}", return_dtype=pl.String
+            ),
+        )
+    )
+    run_corr_matrix_long_df
+    return (run_corr_matrix_long_df,)
+
+
+@app.cell
+def _(alt, corr_sample_size, run_corr_matrix_long_df):
+
+    _heatmap_corr_chart = (
+        alt.Chart(run_corr_matrix_long_df)
+        .mark_rect()
+        .encode(
+            x=alt.X(
+                "var_x:O",
+                title="",
+                axis=alt.Axis(
+                    orient="top",  # move axis to top
+                    labelAngle=-45,  # angle the labels (negative = counterclockwise)
+                ),
+            ),
+            y=alt.Y("var_y:O", title=""),
+            # use spectral theme since we have both negative and positive correlations
+            color=alt.Color("correlation").scale(scheme="spectral"),
+        )
+        .properties(width=450, height=300)
+    )
+
+    _text_corr_chart = _heatmap_corr_chart.mark_text().encode(
+        x="var_x:O",
+        y="var_y:O",
+        text="correlation_str",
+        # for contrast, vary the color based on the intensity of the correlation, negative or positive
+        color=alt.condition(
+            "abs(datum.correlation) > 0.3", alt.value("white"), alt.value("black")
+        ),
+    )
+
+    (_heatmap_corr_chart + _text_corr_chart).properties(
+        width=600,
+        title=f"Payoff / parameter correlation (based on sample of {corr_sample_size:,} agents)",
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
     ### Grid Size
     """)
     return
@@ -248,6 +377,51 @@ def _(alt, custom_boxplot, model_agent_df, payoffchart_title, pl):
 def _(payoff_by_risk_playnhood, plot_mean_quartiles):
     plot_mean_quartiles(
         payoff_by_risk_playnhood, "play_neighborhood", "Play Neighborhood"
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Observed Neighborhood
+    """)
+    return
+
+
+@app.cell
+def _(model_agent_df, pl):
+
+    # calculate mean & quartiles for risk attitude by observed neighborhood
+    payoff_by_risk_obsvnhood = (
+        model_agent_df.group_by("risk_attitude", "observed_neighborhood")
+        .agg(
+            min=pl.col("scaled_points").min(),
+            max=pl.col("scaled_points").max(),
+            median=pl.col("scaled_points").median(),
+            mean=pl.col("scaled_points").mean(),
+            Q1=pl.col("scaled_points").quantile(0.25),
+            Q2=pl.col("scaled_points").quantile(0.5),
+            Q3=pl.col("scaled_points").quantile(0.75),
+        )
+        .sort("observed_neighborhood", "risk_attitude")
+    )
+
+    return (payoff_by_risk_obsvnhood,)
+
+
+@app.cell
+def _(alt, custom_boxplot, payoff_by_risk_obsvnhood, payoffchart_title):
+    custom_boxplot(payoff_by_risk_obsvnhood).facet(
+        column=alt.Column("observed_neighborhood", title="Observed Neighborhood")
+    ).properties(title=payoffchart_title)
+    return
+
+
+@app.cell
+def _(payoff_by_risk_obsvnhood, plot_mean_quartiles):
+    plot_mean_quartiles(
+        payoff_by_risk_obsvnhood, "observed_neighborhood", "Observed Neighborhood"
     )
     return
 
